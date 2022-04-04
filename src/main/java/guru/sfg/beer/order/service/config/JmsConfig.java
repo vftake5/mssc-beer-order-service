8e876bbc84a0e4aa52989e0c1f70821ac7e28cbd
vftake5 <vftake5@gmail.com>
2022-04-04 11:44:04 +0200

    Config JMS. Closes #5

---
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -0,0 +1,19 @@
+package guru.sfg.beer.order.service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+public class JmsConfig
+{
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter()
+	{
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+
+		return converter;
+	}
+}
